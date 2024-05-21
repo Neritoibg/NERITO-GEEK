a7761e9a0451a4269d0f071fd7fcf9561bb28497
nerito <camiloibag057@gmail.com>
2024-05-21 11:45:02 -0500

    correcion de codigo y puesto delete

--- js/conexionAPI.js
+++ js/conexionAPI.js
@@ -1,72 +1,112 @@
-async function listarProductos() {
-    const conexion = await fetch("http://localhost:3000/productos/");
+// async function listarProductos() {
+//     const conexion = await fetch("http://localhost:3000/productos/");
 
-    const conexionConvertida = conexion.json();
+//     const conexionConvertida = conexion.json();
 
-    return conexionConvertida
-}
+//     return conexionConvertida
+// }
 
-async function enviarProducto(nombre, precio, imagen) {
-    const conexion = await fetch("http://localhost:3000/productos/", {
-        method: "POST",
-        headers: { "content-type": "application/json" },
-        body: JSON.stringify({
-            nombre: nombre,
-            precio: precio,
-            imagen: imagen
-        })
-    });
+// async function enviarProducto(nombre, precio, imagen) {
+//     const conexion = await fetch("http://localhost:3000/productos/", {
+//         method: "POST",
+//         headers: { "content-type": "application/json" },
+//         body: JSON.stringify({
+//             nombre: nombre,
+//             precio: precio,
+//             imagen: imagen
+//         })
+//     });
 
-    const conexionConvertida = conexion.json();
+//     const conexionConvertida = conexion.json();
 
-    return conexionConvertida;
-}
-async function eliminarProducto(id) {
-    try {
-        const response = await fetch(`http://localhost:3000/productos/${id}`, {
-            method: 'DELETE',
-            headers: {
-                "Content-Type": "application/json",
-            },
-        });
+//     return conexionConvertida;
+// }
+// async function eliminarProducto(id) {
+//     try {
+//         const response = await fetch(`http://localhost:3000/productos/${id}`, {
+//             method: 'DELETE',
+//             headers: {
+//                 "Content-Type": "application/json",
+//             },
+//         });
         
-        if (response.ok) {
-            console.log('Producto eliminado exitosamente');
-            // Aquí puedes actualizar la interfaz de usuario para reflejar el producto eliminado si es necesario
-        } else {
-            console.error('Error al eliminar el producto:', response.status);
-            // Aquí puedes manejar el error de alguna manera apropiada
-        }
-    } catch (error) {
-        console.error('Error al eliminar el producto:', error);
-        // Aquí puedes manejar el error de alguna manera apropiada
-    }
-}
+//         if (response.ok) {
+//             console.log('Producto eliminado exitosamente');
+//             // Aquí puedes actualizar la interfaz de usuario para reflejar el producto eliminado si es necesario
+//         } else {
+//             console.error('Error al eliminar el producto:', response.status);
+//             // Aquí puedes manejar el error de alguna manera apropiada
+//         }
+//     } catch (error) {
+//         console.error('Error al eliminar el producto:', error);
+//         // Aquí puedes manejar el error de alguna manera apropiada
+//     }
+// }
 
-// Event listener para el icono de eliminar en el card del producto
-document.addEventListener('click', async function(event) {
-    if (event.target.classList.contains('basura')) {
-        // Obtener el card del producto más cercano al botón de eliminar
-        const cardProducto = event.target.closest('.contenedorProductos');
-        // Verificar si se encontró un card de producto
-        if (cardProducto) {
-            // Obtener el ID del producto a partir del atributo data-id en el card
-            const id = cardProducto.dataset.id;
+// // Event listener para el icono de eliminar en el card del producto
+// document.addEventListener('click', async function(event) {
+//     if (event.target.classList.contains('basura')) {
+//         // Obtener el card del producto más cercano al botón de eliminar
+//         const cardProducto = event.target.closest('.contenedorProductos');
+//         // Verificar si se encontró un card de producto
+//         if (cardProducto) {
+//             // Obtener el ID del producto a partir del atributo data-id en el card
+//             const id = cardProducto.dataset.id;
             
-            // Confirmar la eliminación del producto
-            if (confirm('¿Estás seguro de que quieres eliminar este producto?')) {
-                // Llamar a la función para eliminar el producto
-                await eliminarProducto(id);
-                // Eliminar el card del producto del DOM
-                cardProducto.remove();
-            }
-        } else {
-            console.error('No se encontró el card de producto asociado al botón de eliminar.');
-        }
-    }
-});
+//             // Confirmar la eliminación del producto
+//             if (confirm('¿Estás seguro de que quieres eliminar este producto?')) {
+//                 // Llamar a la función para eliminar el producto
+//                 await eliminarProducto(id);
+//                 // Eliminar el card del producto del DOM
+//                 cardProducto.remove();
+//             }
+//         } else {
+//             console.error('No se encontró el card de producto asociado al botón de eliminar.');
+//         }
+//     }
+// });
 
 
+// export const conexionAPI = {
+//     listarProductos, enviarProducto, eliminarProducto
+// }
+
+const apiUrl = "http://localhost:3000/productos/";
+
+// Funciones para la página web
+
+const productList = () => {
+    return fetch(apiUrl)
+        .then((res) => res.json())
+        .catch((err) => console.log(err));
+};
+
+const createProducts = (nombre, precio, imagen) => {
+    return fetch(apiUrl, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            nombre,
+            precio,
+            imagen,
+        }),
+
+    })
+        .then((res) => res.json())
+        .catch((err) => console.log(err));
+};
+
+const deleteProduct = (id) => {
+    return fetch(`${apiUrl}/${id}`, {
+        method: "DELETE"
+    })
+        .then(res => res.json())
+        .catch((err) => console.log(err));
+};
 export const conexionAPI = {
-    listarProductos, enviarProducto, eliminarProducto
+    productList,
+    createProducts,
+    deleteProduct,
 }
--- js/mostrarProductos.js
+++ js/mostrarProductos.js
@@ -2,28 +2,67 @@ import { conexionAPI } from "./conexionAPI.js";
 
 
 const lista = document.querySelector("[data-lista]")
+const formulario = document.querySelector("[data-formulario]");
 
 function crearCard(nombre, precio, imagen, id) {
     const producto = document.createElement("li");
-    producto.className = "producto__item";
-    producto.setAttribute("data-id", id);
+    producto.classList.add("producto-Geek");
     producto.innerHTML =`    <div class="imgProducto">
     <img src="${imagen}" alt="posillo blanco trooper">
 </div>
 <div class="DescripcionProducto">
     <h3>${nombre}</h3>
-    <img class="basura" src="../img/eliminar.png" alt="icono basura" id="${id}" data-delete >
+    <img class="basura" data-id="${id}" src="../img/eliminar.png" alt="icono basura" >
     <p>${precio}</p>
 </div>`;
 
-    return producto;
+const deleteButton = producto.querySelector(".basura");
+deleteButton.addEventListener("click", () => {
+    conexionAPI.deleteProduct(id)
+        .then(() => {
+            producto.remove();
+        })
+        .catch(err => console.log(err));
+});
+
+lista.appendChild(producto);
+return producto;
+
 }
 
-async function listarProductos() {
-    const listaAPI = await conexionAPI.listarProductos()
+const render = async () => {
+    try {
+        const listProducts = await conexionAPI.productList();
+        listProducts.forEach(productos => {
+            lista.appendChild(
+                crearCard(
+                    productos.nombre,
+                    productos.precio,
+                    productos.imagen,
+                    productos.id)
+            );
 
-    listaAPI.forEach(producto => lista.appendChild(crearCard(producto.nombre, producto.precio, producto.imagen, producto.id)))
-}
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
 
-listarProductos();
+formulario.addEventListener("submit", (evento) => {evento.preventDefault();
+
+
+    const nombre=document.querySelector("[data-nombre]").value;
+    const precio=document.querySelector("[data-precio]").value;
+    const imagen=document.querySelector("[data-imagen]").value;
+
+    conexionAPI
+    .createProducts(nombre, precio, imagen)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err))
+});
+
+
+
+render();
+
 
